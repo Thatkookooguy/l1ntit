d259a5a24b12f6273eb23ad097041f03b1199b42
Neil Kalman <Neilkalman@gmail.com>
2018-11-18 15:49:28 +0200

    base functionality without inline comments
    need to understand how to post inline comments using the patch row number of a file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const lintPR = require('./lib/lint-pull-request');
+
 module.exports = app => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log('Yay, the app was loaded!');
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.on("pull_request.opened", lintPR);
+  app.on("pull_request.synchronize", lintPR);
 
   // For more information on building apps:
   // https://probot.github.io/docs/
